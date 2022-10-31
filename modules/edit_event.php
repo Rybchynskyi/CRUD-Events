98caade382b7c3aa6c0f63efa5cf454d2df39747
Rybchynskyi <alexandr.rybchinskij@gmail.com>
2022-10-31 08:22:47 +0200

    delete echo`s

--- modules/edit_event.php
+++ modules/edit_event.php
@@ -8,14 +8,6 @@ $eventLocation = htmlspecialchars($_POST['eventLocation']);
 $eventDateTime = htmlspecialchars($_POST['eventDateTime']);
 $eventType = htmlspecialchars($_POST['eventType']);
 
-echo $eventId . '<br>';
-echo $eventName . '<br>';
-echo $eventDescr . '<br>';
-echo $eventLocation . '<br>';
-echo $eventDateTime . '<br>';
-echo $eventType . '<br>';
-
-
 $sql_edit_event = "UPDATE `events` SET
                       `name` = '$eventName',
                       `descr` = '$eventDescr',
